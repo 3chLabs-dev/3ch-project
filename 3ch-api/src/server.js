85c1e761c61f7dbf1963785b35cb0ca8c39d94ab
SyunChann <chltmdcks3@gmail.com>
2026-02-05 16:54:35 +0900

    Add) package json script 추가 기본 동작주소 스웨거 변경

--- 3ch-api/src/server.js
+++ 3ch-api/src/server.js
@@ -3,5 +3,5 @@ const app = require("./app");
 const PORT = Number(process.env.PORT || 3000);
 
 app.listen(PORT, "127.0.0.1", () => {
-  console.log(`API 동작중 : http://127.0.0.1:${PORT}`);
+  console.log(`API 동작중 : http://127.0.0.1:${PORT}/swagger`);
 });

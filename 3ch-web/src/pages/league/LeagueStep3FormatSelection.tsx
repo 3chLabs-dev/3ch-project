a5e2a66f794d1040abe4888dd3ea3afe26474a0a
NAK\sne91 <sne910608@kakao.com>
2026-02-09 15:34:31 +0900

    Modi) 피그마 양식에 맞춰서 수정

--- 3ch-web/src/pages/league/LeagueStep3FormatSelection.tsx
+++ 3ch-web/src/pages/league/LeagueStep3FormatSelection.tsx
@@ -1,5 +1,14 @@
 import React, { useState } from "react";
-import { Box, Typography, Button, RadioGroup, FormControlLabel, Radio, FormControl, FormLabel } from "@mui/material";
+import { 
+  Box, 
+  Typography, 
+  Button, 
+  RadioGroup, 
+  FormControlLabel, 
+  Radio, 
+  FormControl, 
+  Stack 
+} from "@mui/material";
 import { setStep, setStep3Format } from "../../features/league/leagueCreationSlice";
 import type { LeagueFormatValue } from "../../features/league/leagueCreationSlice";
 import { useAppDispatch, useAppSelector } from "../../app/hooks";
@@ -28,32 +37,95 @@ const LeagueStep3FormatSelection: React.FC = () => {
 
   return (
     <Box sx={{ p: 3, maxWidth: 500, mx: "auto" }}>
-      <Typography variant="h5" fontWeight={900} gutterBottom>
-        리그 방식 선택
+      <Typography sx={{ fontSize: 22, fontWeight: 900, mb: 2 }}>
+        리그 방식
       </Typography>
 
-      <FormControl component="fieldset" margin="normal" fullWidth>
-        <FormLabel component="legend">방식</FormLabel>
+      <FormControl fullWidth>
         <RadioGroup
           aria-label="league-format"
           name="league-format-group"
           value={selectedFormat}
           onChange={(e) => setSelectedFormat(e.target.value as LeagueFormatValue)}
+          sx={{ display: "flex", gap: 2}}
         >
           {LeagueFormatOptions.map((option) => (
-            <FormControlLabel key={option.value} value={option.value} control={<Radio />} label={option.label} />
+            <FormControlLabel 
+              key={option.value} 
+              value={option.value} 
+              label={option.label} 
+              control={<Radio />} 
+              sx={{
+                m: 0,
+                borderRadius: 1,
+                border: "1px solid",
+                borderColor: "grey.300",
+                bgcolor: "background.paper",
+                px: 2,
+                py: 2,
+                boxShadow: 1,
+                gap: 1.5,
+                alignItems: "center",
+
+                "& .MuiFormControlLabel-label": {
+                  fontSize: 20,
+                  fontWeight: 700,
+                },
+
+                ...(selectedFormat === option.value && {
+                  borderColor: "grey.900",
+                  boxShadow: 2,
+                }),
+
+                "& .MuiRadio-root": {
+                  p: 0.5,
+                },
+
+                // hover
+                "&:hover": {
+                  borderColor: "grey.700",
+                },
+              }}
+            />
           ))}
         </RadioGroup>
       </FormControl>
 
-      <Box sx={{ mt: 3, display: "flex", justifyContent: "space-between" }}>
-        <Button variant="outlined" onClick={handlePrev}>
+      <Stack direction="row" spacing={2} sx={{ mt: 4 }}>
+        <Button
+          fullWidth
+          variant="contained"
+          onClick={handlePrev}
+          disableElevation
+          sx={{
+            borderRadius: 1,
+            height: 44,
+            fontWeight: 900,
+            bgcolor: "#777777",
+            "&:hover": { bgcolor: "#777777" },
+          }}
+        >
           이전
         </Button>
-        <Button variant="contained" onClick={handleNext} disabled={!selectedFormat}>
+
+        <Button
+          fullWidth
+          variant="contained"
+          onClick={handleNext}
+          disableElevation
+          disabled={!selectedFormat}
+          sx={{
+            borderRadius: 1,
+            height: 44,
+            fontWeight: 900,
+            bgcolor: "#2F80ED",
+            "&:hover": { bgcolor: "#256FD1" },
+            "&.Mui-disabled": { bgcolor: "#CFE1FB", color: "#fff" },
+          }}
+        >
           다음
         </Button>
-      </Box>
+      </Stack>
     </Box>
   );
 };

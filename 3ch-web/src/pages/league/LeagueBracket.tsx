799106beba92f323731d3580b40eac3517585c8f
NAK\sne91 <sne910608@kakao.com>
2026-02-26 15:54:46 +0900

    Modi) 불필요한 코드 삭제

--- 3ch-web/src/pages/league/LeagueBracket.tsx
+++ 3ch-web/src/pages/league/LeagueBracket.tsx
@@ -9,7 +9,6 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-import { toUTCDate } from "../../utils/dateUtils";
 import { useParams } from "react-router-dom";
 import { formatLeagueDate } from "../../utils/dateUtils";
 import {
